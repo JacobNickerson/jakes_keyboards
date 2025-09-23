110bb7f1b2d9542f358f33d6d344867546b0b5fb
Jacob Nickerson <jacobnickerson817@gmail.com>
2025-09-23 14:18:54 -0400

    Add missing layer key, remove layer blocking from all layers

--- inland/mk47/keymaps/jakes/keymap.c
+++ inland/mk47/keymaps/jakes/keymap.c
@@ -161,35 +161,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(NAV), MO(NUM),      KC_SPC,      MO(FN),  MO(KEB), XXXXXXX, KC_DEL,  KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT, MO(NAV), MO(NUM),      KC_SPC,      MO(FN),  MO(KEB), MO(NAV), KC_DEL,  KC_RCTL
     ),
 
     [NUM] = LAYOUT_planck_mit(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, XXXXXXX, 
-        _______, _______, _______, _______, MO(NUM),     _______,      MO(FN),  XXXXXXX, XXXXXXX, XXXXXXX, _______
+        _______, _______, _______, _______, _______,     _______,      _______,  _______, _______, XXXXXXX, _______
     ),
 
     [NAV] = LAYOUT_planck_mit(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, WN_LEFT, WN_DOWN, WN_UP,   WN_RGHT, XXXXXXX, _______,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______, _______, MO(NAV), XXXXXXX,     _______,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
+        _______, _______, _______, _______, _______,     _______,      _______, _______, _______, XXXXXXX, _______
     ),
 
     [FN] = LAYOUT_planck_mit(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, KC_F12,
         KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, KC_PSCR,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______, _______, _______, _______, MO(NUM),     _______,      MO(FN),  XXXXXXX, XXXXXXX, XXXXXXX, _______
+        _______, _______, _______, _______, _______,     _______,      _______,  _______, _______, XXXXXXX, _______
     ),
 
     [KEB] = LAYOUT_planck_mit(
         RM_RES,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,
         RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         RM_PREV, RM_HUED, RM_SATD, RM_VALD, RM_SPDD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX,      XXXXXXX, MO(KEB), XXXXXXX, XXXXXXX, XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,     XXXXXXX,      _______, _______, _______, XXXXXXX, XXXXXXX
     ),
 
     [SEC] = LAYOUT_planck_mit(
